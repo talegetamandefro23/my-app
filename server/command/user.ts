b6051234bc5b84ad6d836665ae57d757153a0581
talegetamandefro23 <talegetamandefro23@gmail.com>
2025-12-10 10:16:23 +0300

    commit

--- server/command/user.ts
+++ server/command/user.ts
@@ -1,6 +1,6 @@
-import { axiosInstance } from '@/service/axiosService'
-export async function createUser(url:string,data: any) {
-    const response = await axiosInstance.post(`${url}`, data)
-  return response.data;
+// import { axiosInstance } from '@/service/axiosService'
+// export async function createUser(url:string,data: any) {
+//     const response = await axiosInstance.post(`${url}`, data)
+//   return response.data;
  
-}
+// }
--- server/query/users.ts
+++ server/query/users.ts
@@ -1,9 +1,9 @@
-  "use server"
+//   "use server"
   
-import { axiosInstance } from "@/service/axiosService";
-import { apiURL } from "@/service/envService";
+// import { axiosInstance } from "@/service/axiosService";
+// import { apiURL } from "@/service/envService";
 
-export  async function getAllUsers (url :string){
-    const response = await axiosInstance.get(`${url}`)
-    return response.data;
-}
+// export  async function getAllUsers (url :string){
+//     const response = await axiosInstance.get(`${url}`)
+//     return response.data;
+// }
